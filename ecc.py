b10f0488c984045a122fd42a83d17df1c4b82587
oishigyunyu <yamamuuuura@gmail.com>
2021-09-29 23:42:35 +0900

    FieldElementのどっかがまちがってた

--- ecc.py
+++ ecc.py
@@ -22,46 +22,73 @@ class ECCTest(TestCase):
                 Point(x, y, a, b)
 
 class Point:
+
     def __init__(self, x, y, a, b):
         self.a = a
         self.b = b
-        self.x = y
+        self.x = x
         self.y = y
-
         if self.x is None and self.y is None:
             return
-        
         if self.y**2 != self.x**3 + a * x + b:
-            raise ValueError('({}, {}) is not on the curve.'.format(x, y))
-    
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+    # end::source1[]
+
     def __eq__(self, other):
-        return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
+        return self.x == other.x and self.y == other.y \
+            and self.a == other.a and self.b == other.b
 
     def __ne__(self, other):
-        return not(self == other)
+        # this should be the inverse of the == operator
+        return not (self == other)
+
+    def __repr__(self):
+        if self.x is None:
+            return 'Point(infinity)'
+        elif isinstance(self.x, FieldElement):
+            return 'Point({},{})_{}_{} FieldElement({})'.format(
+                self.x.num, self.y.num, self.a.num, self.b.num, self.x.prime)
+        else:
+            return 'Point({},{})_{}_{}'.format(self.x, self.y, self.a, self.b)
 
     def __add__(self, other):
         if self.a != other.a or self.b != other.b:
-            raise ValueError('Points {}, {} are not on the same curve.'.format(self, other))
-        
+            raise TypeError('Points {}, {} are not on the same curve'.format(self, other))
+        # Case 0.0: self is the point at infinity, return other
         if self.x is None:
             return other
-        
+        # Case 0.1: other is the point at infinity, return self
         if other.x is None:
             return self
-        
+
+        # Case 1: self.x == other.x, self.y != other.y
+        # Result is point at infinity
         if self.x == other.x and self.y != other.y:
             return self.__class__(None, None, self.a, self.b)
 
+        # Case 2: self.x ≠ other.x
+        # Formula (x3,y3)==(x1,y1)+(x2,y2)
+        # s=(y2-y1)/(x2-x1)
+        # x3=s**2-x1-x2
+        # y3=s*(x1-x3)-y1
         if self.x != other.x:
             s = (other.y - self.y) / (other.x - self.x)
             x = s**2 - self.x - other.x
             y = s * (self.x - x) - self.y
             return self.__class__(x, y, self.a, self.b)
-        
+
+        # Case 4: if we are tangent to the vertical line,
+        # we return the point at infinity
+        # note instead of figuring out what 0 is for each type
+        # we just use 0 * self.x
         if self == other and self.y == 0 * self.x:
             return self.__class__(None, None, self.a, self.b)
 
+        # Case 3: self == other
+        # Formula (x3,y3)=(x1,y1)+(x1,y1)
+        # s=(3*x1**2+a)/(2*y1)
+        # x3=s**2-2*x1
+        # y3=s*(x1-x3)-y1
         if self == other:
             s = (3 * self.x**2 + self.a) / (2 * self.y)
             x = s**2 - 2 * self.x
@@ -70,56 +97,71 @@ class Point:
 
 
 class FieldElement:
-    def __init__(self, num: int, prime: int):
+
+    def __init__(self, num, prime):
         if num >= prime or num < 0:
-            error = 'Num {} not in field ranfe 0 to {}'.format(num, prime - 1)
+            error = 'Num {} not in field range 0 to {}'.format(
+                num, prime - 1)
             raise ValueError(error)
         self.num = num
         self.prime = prime
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return 'FieldElement_{}({})'.format(self.prime, self.num)
 
-    def __eq__(self, other: any) -> bool:
+    def __eq__(self, other):
         if other is None:
             return False
         return self.num == other.num and self.prime == other.prime
 
-    def __ne__(self, other: any) -> bool:
-        if other is None:
-            return False
-        return self.num != other.num and self.prime != other.prime
+    def __ne__(self, other):
+        # this should be the inverse of the == operator
+        return not (self == other)
 
     def __add__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in differnt Fields.')
+            raise TypeError('Cannot add two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
         num = (self.num + other.num) % self.prime
-        return __class__(num, self.prime)
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
 
     def __sub__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in differnt Fields.')
+            raise TypeError('Cannot subtract two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
         num = (self.num - other.num) % self.prime
-        return __class__(num, self.prime)
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
 
     def __mul__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in differnt Fields.')
+            raise TypeError('Cannot multiply two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
         num = (self.num * other.num) % self.prime
+        # We return an element of the same class
         return self.__class__(num, self.prime)
 
     def __pow__(self, exponent):
-        #  num = (self.num ** exponent) % self.prime
         n = exponent % (self.prime - 1)
         num = pow(self.num, n, self.prime)
         return self.__class__(num, self.prime)
 
-    def __truedev__(self, other):
+    def __truediv__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in differnt Fields.')
-        num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
+            raise TypeError('Cannot divide two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        # use fermat's little theorem:
+        # self.num**(p-1) % p == 1
+        # this means:
+        # 1/n == pow(n, p-2, p)
+        num = (self.num * pow(other.num, self.prime - 2, self.prime)) % self.prime
+        # We return an element of the same class
         return self.__class__(num, self.prime)
-
 
 if __name__ == '__main__':
     prime = 31
