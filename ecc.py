51c28cfe4a73158f7529c6a5975656fbd75bbf87
oishigyunyu <yamamuuuura@gmail.com>
2021-09-30 23:43:48 +0900

    bitcoins ellipse function defined

--- ecc.py
+++ ecc.py
@@ -192,11 +192,35 @@ class FieldElement:
         num = (self.num * coefficient) % self.prime
         return self.__class__(num=num, prime=self.prime)
 
+
 P = 2**256 - 2**32 - 977
+
 
 class S256Field(FieldElement):
     def __init__(self, num, prime=None):
-        super().__init__(num=num, prime=prime)
+        super().__init__(num=num, prime=P)
     
     def __repr__(self):
         return '{:x}'.format(self.num).zfill(64)
+
+
+A = 0
+B = 7
+
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+
+class S256Point(Point):
+    def __init__(self, x, y, a=None, b=None):
+        a, b = S256Field(A), S256Field(B)
+        if type(x) == int:
+            super().__init__(x=S256Field(x), y=S256Field(y), a=a, b=b)
+        else:
+            super().__init__(x=x, y=y, a=a, b=b)
+
+    def __rmul__(self, coefficient):
+        coef = coefficient % N
+        return super().__rmul__(coef)
+
+G = S256Point(
+    0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+    0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
