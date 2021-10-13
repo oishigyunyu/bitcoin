761ab2ec89a50653f254fa713b5303a248f9b084
oishigyunyu <yamamuuuura@gmail.com>
2021-10-13 23:29:46 +0900

    kが一意性を持つような実装に変更

--- ecc.py
+++ ecc.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+import hashlib
 from unittest import TestCase
-from random import randint
-
+import hmac
 class ECCTest(TestCase):
     
     def test_on_curve(self):
@@ -257,8 +257,29 @@ class PrivateKey:
     def hex(self):
         return '{:x}'.format(self.secret).zfill(64)
 
+    def deterministic_k(self, z):
+        k = b'\x00' * 32
+        v = b'\x01' * 32
+        if z > N:
+            z -= N
+        
+        z_bytes = z.to_bytes(32, 'big')
+        secret_bytes = self.secret.to_bytes(32, 'big')
+        s256 = hashlib.sha256
+        k = hmac.new(k, v + b'\x00' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        k = hmac.new(k, v + b'\x01' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        while True:
+            v = hmac.new(k, v, s256).digest()
+            candidate = int.from_bytes(v, 'big')
+            if candidate >= 1 and candidate < N:
+                return candidate  # <2>
+            k = hmac.new(k, v + b'\x00', s256).digest()
+            v = hmac.new(k, v, s256).digest()
+
     def sign(self, z):
-        k = randint(0, N - 1)
+        k = self.deterministic_k(z)
         r = (k * G).x.num
         k_inv = pow(k, N - 2, N)
         s = (z + r * self.secret) * k_inv % N
@@ -267,3 +288,5 @@ class PrivateKey:
             s = N - s
         
         return Signature(r, s)
+
+    
