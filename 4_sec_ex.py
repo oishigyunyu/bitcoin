551a523823b0f433df865c24b0c6a11b446f1808
oishigyunyu <yamamuuuura@gmail.com>
2021-10-17 22:43:27 +0900

    ex4

--- 4_sec_ex.py
+++ 4_sec_ex.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from ecc import PrivateKey, Signature
-
+from helper import encode_base58
 
 a = PrivateKey(5000)
 a = a.point.sec(compressed=False)
@@ -37,4 +37,11 @@ a = a.der()
 print(a.hex())
 
 print('-----------')
-print('ex4')
+h = '7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d'
+print(encode_base58(bytes.fromhex(h)))
+
+h = 'eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c'
+print(encode_base58(bytes.fromhex(h)))
+
+h = 'c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6'
+print(encode_base58(bytes.fromhex(h)))
--- helper.py
+++ helper.py
@@ -2,6 +2,26 @@ from unittest import TestSuite, TextTestRunner
 
 import hashlib
 
+BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+
+def encode_base58(s):
+    count = 0
+    for c in s:
+        if c == 0:
+            count += 1
+        else:
+            break
+    
+    num = int.from_bytes(s, 'big')
+    prefix = '1' * count
+    result = ''
+    while num > 0:
+        num, mod = divmod(num, 58)
+        result = BASE58_ALPHABET[mod] + result
+    
+    return prefix + result
+
 
 def run(test):
     suite = TestSuite()
