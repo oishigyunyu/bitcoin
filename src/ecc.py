5f4447b8c1f2043abdeac1d3a9c56f0673e15aca
oishigyunyu <yamamuuuura@gmail.com>
2022-04-16 20:06:57 +0900

    点どうしの加算の実装とフォーマット

--- src/ecc.py
+++ src/ecc.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Union
+
 
 class FieldElement:
     def __init__(self, num: int, prime: int) -> None:
@@ -12,54 +14,86 @@ class FieldElement:
     def __repr__(self) -> str:
         return "Field Element_{}({})".format(self.prime, self.num)
 
-    def __eq__(self, other):
+    def __eq__(self, other: "FieldElement") -> bool:
         if other is None:
             return False
-        return self.num == other.num and self.prime == other.prime
+        result = self.num == other.num and self.prime == other.prime
+        return result
 
-    def __ne__(self, other):
+    def __ne__(self, other) -> bool:
         return not (self == other)
 
-    def __add__(self, other):
+    def __add__(self, other: "FieldElement") -> "FieldElement":
         if self.prime != other.prime:
             raise TypeError("Cannot add two numbers in differnt Fields")
         num = (self.num + other.num) % self.prime
         return self.__class__(num, self.prime)
 
-    def __sub__(self, other):
+    def __sub__(self, other: "FieldElement") -> "FieldElement":
         if self.prime != other.prime:
             raise TypeError("Cannot add two numbers in differnt Fields")
         num = (self.num - other.num) % self.prime
         return self.__class__(num, self.prime)
 
-    def __mul__(self, other):
+    def __mul__(self, other: "FieldElement") -> "FieldElement":
         if self.prime != other.prime:
             raise TypeError("Cannot multiple two numbers in differnt Fields")
         num = (self.num * other.num) % self.prime
         return self.__class__(num, self.prime)
 
-    def __pow__(self, exponent):
+    def __pow__(self, exponent: int) -> "FieldElement":
         n = exponent % (self.prime - 1)
         num = pow(self.num, n, self.prime)
         return self.__class__(num, self.prime)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: "FieldElement") -> "FieldElement":
         if self.prime != other.prime:
             raise TypeError("Cannot add divide numbers in differnt Fields")
         num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
         return self.__class__(num, self.prime)
 
+
 class Point:
-    def __init__(self, x, y, a, b):
+    def __init__(
+        self,
+        x: Union[int, float, None],
+        y: Union[int, float, None],
+        a: Union[int, float, None],
+        b: Union[int, float, None],
+    ) -> None:
         self.a = a
         self.b = b
         self.x = x
         self.y = y
-        if self.y**2 != self.x**3 + a * x + b:
-            raise ValueError('({}, {}) is not on the curve.'.format(x, y))
-        
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
+        if self.x is None and self.y is None:
+            return
+        if self.y ** 2 != self.x ** 3 + a * x + b:
+            raise ValueError("({}, {}) is not on the curve.".format(x, y))
 
-    def __ne__(self, other):
-        return self.x != other.x and self.y != other.y and self.a != other.a and self.b != other.b
+    def __eq__(self, other: "Point") -> bool:
+        return (
+            self.x == other.x
+            and self.y == other.y
+            and self.a == other.a
+            and self.b == other.b
+        )
+
+    def __ne__(self, other: "Point") -> bool:
+        return (
+            self.x != other.x
+            and self.y != other.y
+            and self.a != other.a
+            and self.b != other.b
+        )
+
+    def __add__(self, other: "Point") -> Union["Point", None]:
+        if self.a != other.a or self.b != other.b:
+            raise TypeError(
+                "Points {}, {} are not on the same curve.".format(self, other)
+            )
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None , self.a, self.b)
--- src/exercise.py
+++ src/exercise.py
@@ -6,7 +6,7 @@ from ecc import Point
 if __name__ == "__main__":
     a = 5
     b = 7
-    #p = Point(2, 4, a, b) 
-    p = Point(-1, -1, a, b) 
-    p = Point(18, 77, a, b) 
-    #p = Point(5, 7, a, b) 
+    # p = Point(2, 4, a, b)
+    p = Point(-1, -1, a, b)
+    p = Point(18, 77, a, b)
+    # p = Point(5, 7, a, b)
