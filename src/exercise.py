a4a8e68fb560f8bcf8b86e95e54db8de6c493d6d
oishigyunyu <yamamuuuura@gmail.com>
2022-04-16 13:44:41 +0900

    練習5

--- src/exercise.py
+++ src/exercise.py
@@ -1,7 +1,13 @@
 from ecc import FieldElement
 
 if __name__ == '__main__':
-    prime = 97
-    print(FieldElement(95, prime) * FieldElement(45, prime) * FieldElement(31, prime))
-    print(FieldElement(17, prime) * FieldElement(13, prime) * FieldElement(19, prime) * FieldElement(44, prime))
-    print((FieldElement(12, prime)**7) * (FieldElement(77, prime)**49))
+    prime = 19
+    ks = [1, 3, 7, 13, 18]
+    max_range = 18
+    for k in ks:
+        field = []
+        for i in range(max_range + 1):
+            field_elem = FieldElement(i, prime)
+            field_elem = field_elem * FieldElement(k, prime)
+            field.append(field_elem.num)
+        print('k={}:'.format(k), field)
