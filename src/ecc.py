920581ca8b8b57c4208dd69dc462ca45b1bcbf0f
oishigyunyu <yamamuuuura@gmail.com>
2022-04-16 17:27:41 +0900

    フォーマットと修正

--- src/ecc.py
+++ src/ecc.py
@@ -1,37 +1,40 @@
+from __future__ import annotations
+
+
 class FieldElement:
-    def __init__(self, num, prime):
+    def __init__(self, num: int, prime: int) -> None:
         if num >= prime or num < 0:
-            error = 'Num {} not in field in range 0 to {}'.format(num, prime - 1)
+            error = "Num {} not in field in range 0 to {}".format(num, prime - 1)
             raise ValueError(error)
         self.num = num
         self.prime = prime
-    
-    def __repr__(self):
-        return 'Field Element_{}({})'.format(self.prime, self.num)
-    
+
+    def __repr__(self) -> str:
+        return "Field Element_{}({})".format(self.prime, self.num)
+
     def __eq__(self, other):
         if other is None:
             return False
         return self.num == other.num and self.prime == other.prime
 
     def __ne__(self, other):
-        return not(self == other)
+        return not (self == other)
 
     def __add__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in differnt Fields')
+            raise TypeError("Cannot add two numbers in differnt Fields")
         num = (self.num + other.num) % self.prime
-        return self.__class__(num, self.prime) 
-    
+        return self.__class__(num, self.prime)
+
     def __sub__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in differnt Fields')
+            raise TypeError("Cannot add two numbers in differnt Fields")
         num = (self.num - other.num) % self.prime
         return self.__class__(num, self.prime)
 
     def __mul__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot multiple two numbers in differnt Fields')
+            raise TypeError("Cannot multiple two numbers in differnt Fields")
         num = (self.num * other.num) % self.prime
         return self.__class__(num, self.prime)
 
@@ -42,7 +45,6 @@ class FieldElement:
 
     def __truediv__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add divide numbers in differnt Fields')
-        other_inv = pow(other.num, self.prime - 2, self.prime)
-        num = (self.num * other_inv.num) % self.prime
+            raise TypeError("Cannot add divide numbers in differnt Fields")
+        num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
         return self.__class__(num, self.prime)
--- src/exercise.py
+++ src/exercise.py
@@ -1,21 +1,22 @@
 from tarfile import FIFOTYPE
+
 from attr import field
 from ecc import FieldElement
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     prime = 31
     a = FieldElement(3, prime)
     b = FieldElement(24, prime)
-    b_inv = b**(b.prime - 2)
+    b_inv = b ** (b.prime - 2)
     result = a * b_inv
     print(result)
     a = FieldElement(17, prime)
-    a_inv = a**(a.prime - 2)
-    result = a_inv**3
+    a_inv = a ** (a.prime - 2)
+    result = a_inv ** 3
     print(result)
     a = FieldElement(4, prime)
-    a_inv = a**(a.prime - 2)
-    a = a_inv**4
+    a_inv = a ** (a.prime - 2)
+    a = a_inv ** 4
     b = FieldElement(11, prime)
     result = a * b
     print(result)
