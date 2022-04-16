a6d6ea6f322e290c96ed2073c0396517a6852e6f
oishigyunyu <yamamuuuura@gmail.com>
2022-04-16 19:11:57 +0900

    secp256k1上の点を生成するPointクラス

--- src/ecc.py
+++ src/ecc.py
@@ -48,3 +48,18 @@ class FieldElement:
             raise TypeError("Cannot add divide numbers in differnt Fields")
         num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
         return self.__class__(num, self.prime)
+
+class Point:
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        if self.y**2 != self.x**3 + a * x + b:
+            raise ValueError('({}, {}) is not on the curve.'.format(x, y))
+        
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
+
+    def __ne__(self, other):
+        return self.x != other.x and self.y != other.y and self.a != other.a and self.b != other.b
--- src/exercise.py
+++ src/exercise.py
@@ -1,22 +1,12 @@
 from tarfile import FIFOTYPE
 
 from attr import field
-from ecc import FieldElement
+from ecc import Point
 
 if __name__ == "__main__":
-    prime = 31
-    a = FieldElement(3, prime)
-    b = FieldElement(24, prime)
-    b_inv = b ** (b.prime - 2)
-    result = a * b_inv
-    print(result)
-    a = FieldElement(17, prime)
-    a_inv = a ** (a.prime - 2)
-    result = a_inv ** 3
-    print(result)
-    a = FieldElement(4, prime)
-    a_inv = a ** (a.prime - 2)
-    a = a_inv ** 4
-    b = FieldElement(11, prime)
-    result = a * b
-    print(result)
+    a = 5
+    b = 7
+    #p = Point(2, 4, a, b) 
+    p = Point(-1, -1, a, b) 
+    p = Point(18, 77, a, b) 
+    #p = Point(5, 7, a, b) 
