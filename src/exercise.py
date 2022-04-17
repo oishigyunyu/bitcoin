17090ab509b8e9d1decc254a9f2a576746e3d9d1
oishigyunyu <yamamuuuura@gmail.com>
2022-04-17 10:33:00 +0900

    3章の練習1

--- src/exercise.py
+++ src/exercise.py
@@ -1,12 +1,17 @@
-from tarfile import FIFOTYPE
+from operator import truediv
+from ecc import Point, FieldElement
 
-from attr import field
-from ecc import Point
+def solve(x: int, y: int) -> bool:
+    prime: int = 223
+    left: int = y**2 % prime
+    right: int = (x**3 + 7) % prime
+    if left == right:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
-    a = 5
-    b = 7
-    # p = Point(2, 4, a, b)
-    p = Point(-1, -1, a, b)
-    p = Point(18, 77, a, b)
-    # p = Point(5, 7, a, b)
+    points = [[192, 105], [17, 56], [200, 119], [1, 193], [42, 99]]
+    for i, point in enumerate(points):
+        result = solve(point[0], point[1])
+        print(i, result)
