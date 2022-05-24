f6c56891e17e31e1973bc48fa381c7e618532f50
oishigyunyu <yamamuuuura@gmail.com>
2022-05-24 21:35:35 +0900

    ビットコイン用の曲線と点Gを定義

--- src/ecc.py
+++ src/ecc.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from tkinter.messagebox import NO
 from typing import Any, Union
 from unittest import TestCase
 
@@ -121,7 +120,12 @@ class Point:
             coef >>= 1
         return result
 
-P = 2**256 - 2**32 -977 
+P = 2**256 - 2**32 - 977
+
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+
+A = 0
+B = 7
 
 class S256Field(FieldElement):
     def __init__(self, num, prime=None):
@@ -130,8 +134,6 @@ class S256Field(FieldElement):
     def __repr__(self):
         return '{:x}'.format(self.num).zfill(64)
 
-A = 0
-B = 7
 
 class S256Point(Point):
     def __init__(self, x, y, a=None, b=None):
@@ -141,6 +143,14 @@ class S256Point(Point):
         
         else:
             super().__init__(x=x, y=y, a=a, b=b)
+    
+    def __rmul__(self, coefficient):
+        coef = coefficient % N
+        return super().__rmul__(coef)
+
+G = S256Point(
+    0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+    0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
 
 
 class ECCTest(TestCase):
