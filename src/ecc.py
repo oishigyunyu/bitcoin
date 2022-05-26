4cebe4aca893dd78f2072bf04be69e1d5196f796
oishigyunyu <yamamuuuura@gmail.com>
2022-05-26 23:40:13 +0900

    べき乗できなくて死ぬ

--- src/ecc.py
+++ src/ecc.py
@@ -88,32 +88,49 @@ class Point:
         else:
             return 'Point({},{})_{}_{}'.format(self.x, self.y, self.a, self.b)
 
-    def __add__(self, other: "Point") -> Union["Point", None]:
+    def __add__(self, other):
         if self.a != other.a or self.b != other.b:
-            raise TypeError(
-                "Points {}, {} are not on the same curve.".format(self, other)
-            )
+            raise TypeError('Points {}, {} are not on the same curve'.format(self, other))
+        # Case 0.0: self is the point at infinity, return other
         if self.x is None:
             return other
+        # Case 0.1: other is the point at infinity, return self
         if other.x is None:
             return self
+
+        # Case 1: self.x == other.x, self.y != other.y
+        # Result is point at infinity
         if self.x == other.x and self.y != other.y:
             return self.__class__(None, None, self.a, self.b)
+
+        # Case 2: self.x ≠ other.x
+        # Formula (x3,y3)==(x1,y1)+(x2,y2)
+        # s=(y2-y1)/(x2-x1)
+        # x3=s**2-x1-x2
+        # y3=s*(x1-x3)-y1
         if self.x != other.x:
-            s: float = (other.y - self.y) / (other.x - self.x)
-            x: float = s ** 2 - self.x - other.x
-            y: float = s * (self.x - x) - self.y
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s**2 - self.x - other.x
+            y = s * (self.x - x) - self.y
             return self.__class__(x, y, self.a, self.b)
-        if self == other:
-            s_eq: float = (3 * self.x ** 2 + self.a) / (2 * self.y)
-            x_eq: float = s_eq ** 2 - 2 * self.x
-            y_eq: float = s_eq * (self.x - x) - self.y
-            return self.__class__(x_eq, y_eq, self.a, self.b)
-        if self == other and self.y == other.y:
+
+        # Case 4: if we are tangent to the vertical line,
+        # we return the point at infinity
+        # note instead of figuring out what 0 is for each type
+        # we just use 0 * self.x
+        if self == other and self.y == 0 * self.x:
             return self.__class__(None, None, self.a, self.b)
-        
-        else:
-            raise NotImplementedError
+
+        # Case 3: self == other
+        # Formula (x3,y3)=(x1,y1)+(x1,y1)
+        # s=(3*x1**2+a)/(2*y1)
+        # x3=s**2-2*x1
+        # y3=s*(x1-x3)-y1
+        if self == other:
+            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            x = s**2 - 2 * self.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
     
     def __rmul__(self, coefficient):
         coef = coefficient
@@ -155,11 +172,11 @@ class S256Point(Point):
         return super().__rmul__(coef)
 
     def verify(self, z, sig):
-        s_inv = pow(sig.s, N-2, N)
+        s_inv = pow(sig.s, N - 2, N)
         u = z * s_inv % N
         v = sig.r * s_inv % N
         total = u * G + v * self
-        return total.x.num == self
+        return total.x.num == sig.r
 
 G = S256Point(
     0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
