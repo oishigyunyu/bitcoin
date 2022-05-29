304b103fa39aa9e188e48dbf06c8ce052eec6368
oishigyunyu <yamamuuuura@gmail.com>
2022-05-29 22:50:57 +0900

    圧縮SECフォーマットの実装

--- src/ecc.py
+++ src/ecc.py
@@ -388,8 +388,14 @@ class S256Point(Point):
         total = u * G + v * self  # <4>
         return total.x.num == sig.r  # <5>
 
-    def sec(self):
-        return b'\x04' + self.x.num.to_bytes(32, 'big') + self.y.num.to_bytes(32, 'big')
+    def sec(self, compressed=True):
+        if compressed:
+            if self.y.num % 2 == 0:
+                return b'\x02' + self.x.num.to_bytes(32, 'big')
+            else:
+                return b'\x03' + self.x.num.to_bytes(32, 'big')
+        else:
+            return b'\x04' + self.x.num.to_bytes(32, 'big') + self.y.num.to_bytes(32, 'big')
     # end::source12[]
 
 
