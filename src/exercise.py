78c444fe140288d3e39c361c62e553d6e5b2424a
oishigyunyu <yamamuuuura@gmail.com>
2022-05-30 22:10:08 +0900

    base58の実装と練習4

--- src/exercise.py
+++ src/exercise.py
@@ -1,7 +1,6 @@
 from ecc import PrivateKey, Signature
-
+from helper import encode_base58
 if __name__ == "__main__":
-    r = 0x37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6
-    s = 0x8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
-    sig = Signature(r, s)
-    print(sig.der().hex())
+    print(encode_base58(bytes.fromhex("7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d")))
+    print(encode_base58(bytes.fromhex("eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c")))
+    print(encode_base58(bytes.fromhex("c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6")))
