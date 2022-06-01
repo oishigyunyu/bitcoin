4c0e3aa28ea4644a643b3817aea1e57767fa7899
oishigyunyu <yamamuuuura@gmail.com>
2022-06-01 22:34:24 +0900

    テストネット上での送金テスト

--- src/exercise.py
+++ src/exercise.py
@@ -1,7 +1,7 @@
 from ecc import PrivateKey, Signature
 from helper import encode_base58
 if __name__ == "__main__":
-    secret = 199705016565
+    secret = "secretsecretsecret"
     priv = PrivateKey(secret)
     pub_address = priv.point.address(compressed=True, testnet=True)
     print(pub_address)
