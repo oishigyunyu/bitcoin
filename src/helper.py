da70e5618411ee9d2cfc3b6ca1224987c8d2aff5
oishigyunyu <yamamuuuura@gmail.com>
2022-06-13 22:41:39 +0900

    TxInクラスの実装

--- src/helper.py
+++ src/helper.py
@@ -40,3 +40,43 @@ def little_endian_to_int(x):
     
 def int_to_little_endian(x, length):
     return x.to_bytes(length, 'little')
+
+def read_variant(s):
+    '''
+    ストリームからの可変長の整数を読み取る
+    '''
+    i = s.read(1)[0]
+    if i == 0xfd:
+        # 0x2dは次の2バイトが整数なのを示す
+        return little_endian_to_int(s.read(2))
+    
+    elif i == 0xfe:
+        # 0x2dは次の4バイトが整数なのを示す
+        return little_endian_to_int(s.read(4))
+
+    elif i == 0xff:
+        # 0x2dは次の8バイトが整数なのを示す
+        return little_endian_to_int(s.read(8))
+    
+    else:
+        # このほかは単なる整数
+        return i
+
+def encode_variant(i):
+    '''
+    整数をvariantとしてエンコードする
+    '''
+    if i < 0xfd:
+        return bytes([i])
+    
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+
+    elif i < 0x100000000:
+        return b'\xfd' + int_to_little_endian(i, 4)
+
+    elif i < 0x10000000000000000:
+        return b'\xfd' + int_to_little_endian(i, 8)
+    
+    else:
+        raise ValueError('integer too large: {}'.format(i))
--- src/tx.py
+++ src/tx.py
@@ -35,3 +35,23 @@ class Tx:
     def parse(cls, s, testnet=False):
         version = little_endian_to_int(s)
         return cls(version, None, None, None, testnet=testnet)
+
+    def serialize(self):
+        raise NotImplementedError
+
+class TxIn:
+    def __init__(self, prev_tx, prev_index, script_sig=None, sequence=0xffffffff) -> None:
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+        if script_sig is None:
+            self.script_sig = Script()
+        else:
+            self.script_sig = script_sig
+        self.sequence = sequence
+    
+    def __repr__(self) -> str:
+        return '{}:{}'.format(self.prev_tx.hex(), self.prev_index,)
+
+class Script:
+    def __init__(self) -> None:
+        raise NotImplementedError
